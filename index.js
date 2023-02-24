9f82e7a8c1c1d7c8624869e285306306b998d0b8
Dylan L <dylancblarocque@gmail.com>
2023-02-24 12:10:18 -0700

    CodeWars Kata 6kyu - longest repetitions

--- index.js
+++ index.js
@@ -21,41 +21,98 @@ function filterStrForLetter(str, letter){
 
 
 function longestRepetition(s) {
-    if (s.length = 0 ){
+    if (s.length === 0 ){
         return ["", 0]
     } else {
-        // function sortStr(str1, str2){
-        //     return str2.length - str1.length || s.indexOf(str1) - s.indexOf(str2)
-        // }
+        let match = s.match(/(\w)\1*/g);
+
         function sorted(arr){
-            for (let i = 0; i < arr.length; i++){
-                for (let j = i; j < arr.length; i++){
+            let noSwaps;
+            for (let i = arr.length; i > 0; i--){
+                noSwaps=true;
+                for (let j = 0; j < i - 1 ; j++){
                     if (arr[j].length > arr[j+1].length){
                         let temp = arr[j];
                         arr[j] = arr[j+1];
                         arr[j+1] = temp;
+                        noSwaps = false;
                     }
                 }
-                console.log(arr);
-                return arr
+                if(noSwaps) break; 
             }
-    
+            return arr ;
         }
         
-        // let match = s.match(/(\w)\1*/g).sort(sortStr);
-        let match = s.match(/(\w)\1*/g);
-        console.log(match);
-
         sorted(match);
     
-        // let char = match[0];
-        // let charLetter = char[0];
-        // let charLength = char.length;
+        let char = match[match.length - 1];
+        let charLetter = char[0];
+        let charLength = char.length;
     
-        // return [charLetter, charLength]
+        return [charLetter, charLength]
     }
 }
 
-console.log(longestRepetition('bbbaaabaaaa'));
+
+function longestRepetitionBest(s) {
+    if (s.length === 0 ){
+        return ["", 0]
+    } else {       
+        function sortStr(str1, str2){
+            return str2.length - str1.length || s.indexOf(str1) - s.indexOf(str2)
+        }
+
+        let match = s.match(/(\w)\1*/g).sort(sortStr);
+        console.log(match);
+    
+        let char = match[0];
+        let charLetter = char[0];
+        let charLength = char.length;
+    
+        return [charLetter, charLength]
+    }
+}
+
+// console.log(longestRepetitionBest('bbbaaabaaaa'));
 // console.log(longestRepetition("aaaabb"));
 // console.log(longestRepetition("aabb"));
+
+
+
+// write a function that output a structure of # where each row number is equal the number of # printed.
+
+function pyramid() {
+    for(let i =0; i < 7; i++){
+        console.log(("#".repeat(i)));
+    }
+}
+pyramid();
+
+// Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+
+//Passing this string to console.log should show something like this:
+
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # #
+
+let size = 8;
+let board = "";
+
+for (let row = 0; row < size; row++) {
+  for (let col = 0; col < size; col++) {
+    if ((row + col) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
