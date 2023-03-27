9f6498b0b9c13e6aab2539b0e90c43dcddd14e8d
Dylan L <dylancblarocque@gmail.com>
2023-03-27 14:01:51 -0700

    ts fucntions

--- ts-intro/hellow-world/src/functions.ts
+++ ts-intro/hellow-world/src/functions.ts
@@ -12,3 +12,10 @@ function calculateTax1(income: number): number {
 function calculateTax3(income: number): void {
   income * 0.2;
 } // this shows the return type
+
+// be sure to turn on
+// "noImplicitReturns": true,
+// "noImplicitParameter": true,
+// to get the compiler to check for implicit returns and implicit parameters cos the compiler will throw an error if you don't annotate the return type and the parameter types
+
+// "noUnusedLocals": true,
