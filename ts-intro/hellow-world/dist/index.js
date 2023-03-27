870258a99aaadc684194d77a734f3707c9e2573d
Dylan L <dylancblarocque@gmail.com>
2023-03-27 12:39:15 -0700

    configure debugging in config

--- ts-intro/hellow-world/dist/index.js
+++ ts-intro/hellow-world/dist/index.js
@@ -1,1 +1,7 @@
 "use strict";
+let age = 25;
+if (age < 30) {
+    age += 10;
+}
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ ts-intro/hellow-world/src/index.ts
@@ -0,0 +1,6 @@
+let age: number = 25;
+
+if (age < 30) {
+  age += 10;
+}
+console.log(age);
